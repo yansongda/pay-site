e8f59848892159bf661ff7c4cf4cc25b094c7d22
yansongda <me@yansongda.cn>
2020-09-17 17:12:06 +0800

    update

---
+++ .vuepress/config.js
@@ -0,0 +1,98 @@
+module.exports = {
+  title: 'Pay',
+  description: '让支付开发更简单',
+  themeConfig: {
+    logo: '/images/logo.png',
+    smoothScroll: true,
+    nav: [
+      { text: 'Home', link: '/' },
+      {
+        text: '版本',
+        items: [
+          { text: 'Version', items: [
+            { text: 'v1.x', link: '/docs/v1/'},
+            { text: 'v2.x', link: '/docs/v2/'}
+          ]}
+        ]
+      },
+      { text: 'Github', link: 'https://github.com/yansongda/pay' },
+    ],
+    sidebar: {
+      '/docs/v1/': [
+        ''
+      ],
+      '/docs/v2/': [
+        {
+          title: 'Get Started', // 必要的
+          path: '/docs/v2/', // 可选的, 标题的跳转链接，应为绝对路径且必须存在
+          collapsable: true, // 可选的, 默认值是 true,
+          sidebarDepth: 1, // 可选的, 默认值是 1
+          children: [
+            ['installation', '安装'],
+            ['quickUsage', '快速上手'],
+            ['contribute', '贡献']
+          ]
+        },
+        {
+          title: '支付 - 支付宝', // 必要的
+          path: '/docs/v2/alipay/', // 可选的, 标题的跳转链接，应为绝对路径且必须存在
+          collapsable: true, // 可选的, 默认值是 true,
+          sidebarDepth: 1, // 可选的, 默认值是 1
+          children: [
+            ['alipay/pay', '支付'],
+            ['alipay/find', '查询'],
+            ['alipay/cancel', '取消'],
+            ['alipay/close', '关闭'],
+            ['alipay/refund', '退款'],
+            ['alipay/verify', '验证服务端信息'],
+            ['alipay/return', '返回']
+          ]
+        },
+        {
+          title: '支付 - 微信', // 必要的
+          path: '/docs/v2/wechat/', // 可选的, 标题的跳转链接，应为绝对路径且必须存在
+          collapsable: true, // 可选的, 默认值是 true,
+          sidebarDepth: 1, // 可选的, 默认值是 1
+          children: [
+            ['wechat/pay', '支付'],
+            ['wechat/find', '查询'],
+            ['wechat/cancel', '取消'],
+            ['wechat/close', '关闭'],
+            ['wechat/refund', '退款'],
+            ['wechat/verify', '验证服务端信息'],
+            ['wechat/return', '返回']
+          ]
+        },
+        {
+          title: '事件系统', // 必要的
+          path: '/docs/v2/events/', // 可选的, 标题的跳转链接，应为绝对路径且必须存在
+          collapsable: true, // 可选的, 默认值是 true,
+          sidebarDepth: 1, // 可选的, 默认值是 1
+          children: [
+            ['events/class', '事件类'],
+            ['events/usage', '使用方式']
+          ]
+        },
+        {
+          title: '日志系统', // 必要的
+          // path: '/v2/logger/', // 可选的, 标题的跳转链接，应为绝对路径且必须存在
+          collapsable: true, // 可选的, 默认值是 true,
+          sidebarDepth: 1, // 可选的, 默认值是 1
+          children: [
+            ['logger/usage', '使用方式']
+          ]
+        },
+        {
+          title: '其它', // 必要的
+          // path: '/v2/others/', // 可选的, 标题的跳转链接，应为绝对路径且必须存在
+          collapsable: true, // 可选的, 默认值是 true,
+          sidebarDepth: 1, // 可选的, 默认值是 1
+          children: [
+            ['others/faq', 'FAQ']
+          ]
+        }
+      ]
+    },
+    lastUpdated: 'Last Updated'
+  }
+}
---
+++ README.md
@@ -0,0 +1,26 @@
+---
+home: true
+heroImage: '/images/logo.png'
+heroText: null
+tagline: null
+actionText: 快速上手 →
+actionLink: /docs/v2/
+features:
+- title: 简洁至上
+  details: 让我们用最少的代码写最复杂的功能，把更多时间花在游戏上。
+- title: 集成最新API
+  details: 集成支付宝、微信最新API，与时俱进不用担心老代码没人维护了。
+- title: 统一标准
+  details: 符合最新 PSR 标准，方便你集成到各种其他框架中，无惧束缚。
+- title: 丰富的事件系统
+  details: 可以 hook 到绝大部分场景，自己的代码自己掌握。
+- title: 命名不那么乱七八糟
+  details: 命名统一，终于可以不用记那些烂七八糟的名称了。
+- title: 文件结构清晰
+  details: 文件结构清晰，架构明了，欢迎 PR。
+footer: MIT Licensed | Copyright © 2018-now Yansongda
+---
+
+### 鸣谢
+
+本站由 [![又拍云](/images/upyun.png)](https://www.upyun.com/?utm_source=lianmeng&utm_medium=referral) 提供 CDN加速/云存储 服务
